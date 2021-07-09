20fc5634048efeda6d5e1cd5b3562a2399468d67
arellanon <nahuel.arellano@gmail.com>
2021-07-09 11:08:16 -0300

    avance en kivy graficos

--- animation-2/test.py
+++ animation-2/test.py
@@ -9,6 +9,10 @@ from kivy.app import App
 from kivy.uix.floatlayout import FloatLayout
 from kivy.lang import Builder
 from kivy.animation import Animation
+from kivy.uix.widget import Widget
+from kivy.properties import ListProperty
+from kivy.clock import Clock
+from kivy.core.window import Window
 
 Builder.load_file('test.kv')
 #Builder.load_string(kv_string)
@@ -24,7 +28,12 @@ class MyWidget(FloatLayout):
             background_color=(0,0,1,1),
             duration=3)
         
-#        self.ids.left.pos_hint["x"] += 0.1 
+        animate += Animation( height = 200,
+                              duration=.5)        
+        
+        print(self.ids.left.height)
+        self.ids.left.height+=0.1
+        #self.ids.left.size_hint["x"] += 0.1 
 #        pos_hint["x"] += 0.1
         #print( "out: ", self.ids['wid1'] )
         
@@ -43,11 +52,22 @@ class MyWidget(FloatLayout):
         # Start the animation
         animate.start(widget)
 """
-class Wid1(FloatLayout):
-    pass
+class left(Widget):
+    velocity = ListProperty([10, 15])
 
-class Wid2(FloatLayout):
-    pass
+    def __init__(self, **kwargs):
+        super(left, self).__init__(**kwargs)
+        Clock.schedule_interval(self.update, 1/60.)
+
+    def update(self, *args):
+        self.x += self.velocity[0]
+        self.y += self.velocity[1]
+
+        if self.x < 0 or (self.x + self.width) > Window.width:
+            self.velocity[0] *= -1
+        if self.y < 0 or (self.y + self.height) > Window.height:
+            self.velocity[1] *= -1
+
 
 class TestApp(App):
     def build(self):
